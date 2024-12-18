18bc4ea077153fe715bc8dfa936a1ea5bcd523fc
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2024-12-19 01:57:17 +0200

    #Improvment: Melhoria nos icons

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -59,32 +59,34 @@
                     </div>
                 </li>
                 <!-- Jobs -->
-                <li class="nav-item">
-                    <a class="nav-link"  href="{{ route('job_posts.index') }}">
-                        <span class="nav-link-icon d-md-none d-lg-inline-block">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                <path d="M5 3v18h14v-18h-14z" />
-                                <path d="M5 6h14" />
-                                <path d="M5 9h14" />
-                            </svg>
-                        </span>
-                        <span class="nav-link-title">Vagas</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('feed.index') }}">
-                        <span class="nav-link-icon d-md-none d-lg-inline-block">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                <path d="M5 3v18h14v-18h-14z" />
-                                <path d="M5 6h14" />
-                                <path d="M5 9h14" />
-                            </svg>
-                        </span>
-                        <span class="nav-link-title">Feed</span>
-                    </a>
-                </li>
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('job_posts.index') }}">
+        <span class="nav-link-icon d-md-none d-lg-inline-block">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M6 3h12l-2 7h-8z" />
+                <path d="M5 21h14a2 2 0 0 0 2 -2v-7a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v7a2 2 0 0 0 2 2z" />
+            </svg>
+        </span>
+        <span class="nav-link-title">Vagas</span>
+    </a>
+</li>
+
+<!-- Feed -->
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('feed.index') }}">
+        <span class="nav-link-icon d-md-none d-lg-inline-block">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M3 5h18" />
+                <path d="M3 12h12" />
+                <path d="M3 19h6" />
+            </svg>
+        </span>
+        <span class="nav-link-title">Feed</span>
+    </a>
+</li>
+
                 <!-- Messages -->
                 <li class="nav-item">
                     <a class="nav-link" href="./messages">
